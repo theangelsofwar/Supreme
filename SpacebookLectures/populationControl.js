d3a52be2e8aef97d7edb01323ac6748dccdb8aed
theangelsofwar <angiechangpagne@gmail.com>
2020-06-15 13:43:41 -0700

    slow sums

--- SpacebookLectures/populationControl.js
+++ SpacebookLectures/populationControl.js
@@ -1,2 +1,26 @@
 //Given an array of birth year and death year representing people, find the year with the highest population
 
+//sliding window, overlapping subproblems: use memoization
+
+//the array is sorted 
+//[People] People: [Birthyear, Deathyear]
+
+
+/**
+ * 
+ * @param {*} Humans 
+ * @return String the (could be year window [start. end] in that case jsut get the first maximum)
+ */
+const populationControl = (Humans) => {
+  let result = "";
+
+  //it could be a range, a window that the highest population exists....
+
+  let memo = new Map();
+
+
+  return result;
+}
+
+
+export default populationControl;
