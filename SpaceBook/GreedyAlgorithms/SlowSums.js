40495b05f532629074a6ab77251c66eacad84340
theangelsofwar <angiechangpagne@gmail.com>
2020-06-15 14:48:49 -0700

    total t

--- SpaceBook/GreedyAlgorithms/SlowSums.js
+++ SpaceBook/GreedyAlgorithms/SlowSums.js
@@ -18,3 +18,31 @@ First, add 4 + 3 for a penalty of 7. Now the array is [7, 2, 1]
 Add 7 + 2 for a penalty of 9. Now the array is [9, 1]
 Add 9 + 1 for a penalty of 10. The penalties sum to 26.
  */
+
+ // Add any extra import statements you may need here
+
+
+// Add any helper functions you may need here
+var startContext = false; 
+
+function getTotalTime(arr) {
+  // we want an innefficiency, therefore we want a maximum number of a. steps and b. cost per step, the aggregate will be the return  
+  if(!arr || arr.length===0) {
+    return 0;
+  } else if(arr.length===1) {
+    return 0;
+  } else {
+    if(!startContext) { 
+      arr.sort();
+      startContext = true;
+    }
+    console.log('line 21 arr is:', arr); //now that it's sorted, we only have to add the sum/map reduce until we reach the end 
+    let localGasPrice = arr.pop();
+    localGasPrice += arr.pop();
+    //arr[arr.length-1] + arr[arr.length-2] + 0; //reassign variables, immutable arrays, //arr = arr.slice(0, arr.length-2);
+    console.log('sliced arr before add other is', arr);
+    arr.push(localGasPrice);
+    return localGasPrice + getTotalTime(arr);
+  }   
+}
+
