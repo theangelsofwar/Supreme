97dfe4e08a49c141eb33f254b3f35128b562dcab
theangelsofwar <angiechangpagne@gmail.com>
2020-07-05 18:48:58 -0700

    myelin sheath

---
+++ 5PathsToShortestPaths/DataStructure/AdjacencyListMatrix.js
@@ -0,0 +1,10 @@
+const AdjacencyListMatrix = () => {
+  const adjList = new Map();
+  //turn map to set
+  adjList.set(NODE_ID, new Set([]));
+
+
+  return adjList;
+}
+
+export default AdjacencyListMatrix;
